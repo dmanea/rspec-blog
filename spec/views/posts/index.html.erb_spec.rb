12c4ad6544809e06a4e9b7b8408180296903ddb8
Davide Manea <hello@dmanea.com>
2015-08-19 16:44:19 +0200

    scaffold post controller

---
+++ spec/views/posts/index.html.erb_spec.rb
@@ -0,0 +1,14 @@
+require 'rails_helper'
+
+RSpec.describe "posts/index", type: :view do
+  before(:each) do
+    assign(:posts, [
+      create(:post),
+      create(:post)
+    ])
+  end
+
+  it "renders a list of posts" do
+    render
+  end
+end
