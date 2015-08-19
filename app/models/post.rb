231b65ed596a76ff16d4535effa31df04b9d6a12
Davide Manea <hello@dmanea.com>
2015-08-19 14:46:58 +0200

    post validations

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,7 @@
 class Post < ActiveRecord::Base
+
+  validates_presence_of :title, :description
+
+  scope :published, ->    { where(published: true) }
+  scope :sorted, ->       { order(post_date: :desc) }
 end
--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,5 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe Post, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  let(:post)  { Post.new }
+
+  describe '#valid?' do
+    
+    it 'should not valid after initialization' do
+      expect(post.valid?).to be false
+    end
+
+    it 'should be valid with only title and description' do
+      post.title        = 'test'
+      post.description  = 'test'
+      expect(post.valid?).to be true
+    end
+  end
+
 end
